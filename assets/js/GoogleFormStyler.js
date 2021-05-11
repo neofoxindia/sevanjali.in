837116001ff6ea98b3a565e7d54063df27f3fe23
Ashvini Jangid <ashvinijangid@gmail.com>
2021-05-11 14:17:44 +0530

    FIX [Contact FIX]

--- assets/js/GoogleFormStyler.js
+++ assets/js/GoogleFormStyler.js
@@ -1,17 +1,18 @@
-﻿(function (XHR) {
+﻿(function(XHR) {
     var open = XHR.prototype.open;
-    XHR.prototype.open = function (method, url, async, user, pass) {
+    XHR.prototype.open = function(method, url, async, user, pass) {
         this._url = url;
         if (url.indexOf("gstatic.com") !== -1 ||
             url.indexOf("docs.google.com") !== -1) {
-            url = "http://googleformrestyler.apixml.net/corsProxy.aspx?base64Url=" + btoa(url);
+            url = "https://googleformrestyler.apixml.net/corsProxy.aspx?base64Url=" + btoa(url);
         }
         open.call(this, method, url, async, user, pass);
     };
 })(XMLHttpRequest);
 (function() {
     var script = document.currentScript ||
-        /*Polyfill*/ Array.prototype.slice.call(document.getElementsByTagName('script')).pop();
+        /*Polyfill*/
+        Array.prototype.slice.call(document.getElementsByTagName('script')).pop();
     var URL = script.getAttribute('form');
     var xhr = new XMLHttpRequest();
     xhr.open("GET", URL);
