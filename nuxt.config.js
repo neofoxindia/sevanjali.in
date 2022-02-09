4f187d606bc3e31557ffa26a80e5c11f71e4ef7c
Ashvini Jangid <ashvinijangid@gmail.com>
2022-02-10 01:13:10 +0530

    UI changed

--- nuxt.config.js
+++ nuxt.config.js
@@ -51,6 +51,9 @@ export default {
     modules: [],
     // Build Configuration: https://go.nuxtjs.dev/config-build
     build: {},
+    router:{
+        base: '/sevanjali.in/'
+    },
     generate: {
         dir: 'docs'
     }
